4b4427868854f333b857bbd1b8fdd0f0e65a9dc5
Benjamin Perry <>
2022-05-30 23:56:33 +0000

    Initial commit

---
+++ ship.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Ship():
+  def__init__(self, screen):
+    """Initialize the ship image and set its starting position"""
+  self.screen = screen
+
+  #load the ship image and get its rect.
+  self.image = pygame.image.load('images/ship.bmp')
+  self.rect = self.image.get rect()
+  self.screen_rect = self.screen_rect.bottom
+  
