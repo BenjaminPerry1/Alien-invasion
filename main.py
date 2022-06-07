40950835870e5ce8024e6153b337a51a2fa7feb8
Benjamin Perry <106564204+BenjaminPerry1@users.noreply.github.com>
2022-06-07 00:29:54 +0000

    fixed a few lines of code but still got some errors

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+
 def run_game():
 #initialize game and create a screen object.
   #Initialize pygame, settings, and screen object.
   pygame.init()
   ai_settings = Settings()
-  (ai_settings.screen_width, ai_settings.screen_height)
-  screen = pygame.display.set_mode((ai_settings.screen_width,                         ai_settings.screen_height))
+  
+  screen = pygame.display.set_mode((ai_settings.screen_width,                           ai_settings.screen_height))
   pygame.display.set_caption("Alien Invasion")
   #Make a ship.ship
   ship = Ship(screen)
@@ -24,6 +25,7 @@ def run_game():
           sys.exit()
     #Make the most recently drawn screen visible.
       pygame.display.flip()
+      
 run_game()
 
     
--- ship.py
+++ ship.py
@@ -2,7 +2,6 @@ import pygame
 
 class Ship():
   def __init__(self, screen):
-    """Initialize the ship image and set its starting position"""
     self.screen = screen
     #load the ship image and get its rect.
     self.image = pygame.image.load('images/ship.bmp')
@@ -12,6 +11,5 @@ class Ship():
     self.rect.centerx = self.screen_rect.centerx
     self.rect.bottom = self.screen_rect.bottom
   def blitme(self):
-    """Draw the ship at its current location."""
     self.screen.blit(self.image, self.rect)
   
